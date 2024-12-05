25baf16664bef606933b5dedfdba60f9eba97cba
Zalax <contact@zalax.xyz>
2024-12-06 00:28:45 +0100

    feat: add podcasts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {NotFoundComponent} from './shared/not-found/not-found.component';
+import {PodcastsComponent} from './pages/podcasts/podcasts.component';
 
 export const routes: Routes = [
   {
@@ -12,7 +13,10 @@ export const routes: Routes = [
     path: 'home',
     component: HomeComponent,
   },
-
+  {
+    path: "podcast",
+    component: PodcastsComponent
+  },
   {
     path: '**',
     component: NotFoundComponent
---
+++ src/app/models/podcast.ts
@@ -0,0 +1,7 @@
+export interface Podcast {
+  id: number,
+  title: string,
+  description: string,
+  duration: number,
+  audioFile: string
+}
