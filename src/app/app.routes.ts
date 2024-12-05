95f55f77fd321462849b53e63515454249202ccc
julien <chadow.video@gmail.com>
2024-12-05 20:05:12 +0100

    feat: remove unused components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
-import {LoginComponent} from './pages/login/login.component';
-import {disconnectedGuard} from './guards/disconnected.guard';
 import {NotFoundComponent} from './shared/not-found/not-found.component';
 
 export const routes: Routes = [
@@ -14,11 +12,7 @@ export const routes: Routes = [
     path: 'home',
     component: HomeComponent,
   },
-  {
-    path: 'login',
-    component: LoginComponent,
-    canActivate: [disconnectedGuard],
-  },
+
   {
     path: '**',
     component: NotFoundComponent
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,41 +1,13 @@
-import { Component } from '@angular/core';
-import {Router, RouterLink} from '@angular/router';
-import {AuthService} from '../../services/auth.service';
-import {ERole} from '../../models/user.dto';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [
-    RouterLink
-  ],
+
   templateUrl: './header.component.html',
+  standalone: true,
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
-  public isLogged = false;
-  public isAdmin = false;
 
-  constructor(
-    private authService: AuthService,
-    private router: Router,
-  ) {
-  }
-
-  ngOnInit() {
-    this.authService.authStateChanged.subscribe((logged: boolean) => {
-      this.isLogged = this.authService.isLoggedIn();
-      this.isAdmin = this.authService.hasRole(ERole.ROLE_ADMIN);
-    });
-  }
-
-  logout() {
-    this.authService.logout();
-    this.router.navigate(['/']);
-  }
-
-
-  disconnect() {
-    this.authService.logout();
-  }
 }
